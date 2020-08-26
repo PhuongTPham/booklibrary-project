717d4778b1196dd03236aae9fe7190471e259cba
Pham Thi Phuong <phuongpham98ptit@gmail.com>
2020-08-26 16:47:52 +0700

    Updates

---
+++ precache-manifest.23925266c2535c87dadf781e29ec50f7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ce0c1a8c919f3eb40ed9ee82478bfb",
+    "url": "/booklibrary-project/index.html"
+  },
+  {
+    "revision": "db46c510f758e3b3a6cb",
+    "url": "/booklibrary-project/static/css/2.c83e3fd9.chunk.css"
+  },
+  {
+    "revision": "b8d3e9261919aeb78dc3",
+    "url": "/booklibrary-project/static/css/main.0e84bdcc.chunk.css"
+  },
+  {
+    "revision": "db46c510f758e3b3a6cb",
+    "url": "/booklibrary-project/static/js/2.2f93fe5a.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/booklibrary-project/static/js/2.2f93fe5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d3e9261919aeb78dc3",
+    "url": "/booklibrary-project/static/js/main.91905c7a.chunk.js"
+  },
+  {
+    "revision": "2d3b0adfda6d85f73307",
+    "url": "/booklibrary-project/static/js/runtime-main.692cc8af.js"
+  },
+  {
+    "revision": "3f30dadaa8f1b69c4e8ac7c503c22d69",
+    "url": "/booklibrary-project/static/media/1280px-Open_Library_tight_logo.svg.3f30dada.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/booklibrary-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/booklibrary-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/booklibrary-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/booklibrary-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/booklibrary-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/booklibrary-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "d57e8fb06566933dc2b993cf5bbfd57e",
+    "url": "/booklibrary-project/static/media/goodreads-choice-awards-2019.d57e8fb0.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/booklibrary-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/booklibrary-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/booklibrary-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/booklibrary-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/booklibrary-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/booklibrary-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/booklibrary-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/booklibrary-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/booklibrary-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/booklibrary-project/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "e39b585bb77f8423380f7da08f5bab33",
+    "url": "/booklibrary-project/static/media/shopping-cart.e39b585b.svg"
+  }
+]);
